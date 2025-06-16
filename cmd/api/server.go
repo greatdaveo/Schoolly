d6c5bdecb2cf7821b13fa0365685c275adda5819
David Olowomeye <olowomeyedavid242@gmail.com>
2025-06-16 15:17:22 +0100

    Tried the old way of using the Get Method for getting all, filtering, and for one entity

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,25 +2,121 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"strconv"
+	"strings"
 
 	mw "github.com/greatdaveo/Schoolly/internal/api/middlewares"
 )
+
+type Teacher struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Class     string
+	Subject   string
+}
+
+var (
+	teachers = make(map[int]Teacher)
+	// mutex = &sync.Mutex{}
+	nextID = 1
+)
+
+// To initialize some dummy data
+func init() {
+	teachers[nextID] = Teacher{
+		ID:        nextID,
+		FirstName: "John",
+		LastName:  "Doe",
+		Class:     "9A",
+		Subject:   "Math",
+	}
+	nextID++
+	teachers[nextID] = Teacher{
+		ID:        nextID,
+		FirstName: "Jane",
+		LastName:  "Smith",
+		Class:     "10A",
+		Subject:   "Biology",
+	}
+	nextID++
+	teachers[nextID] = Teacher{
+		ID:        nextID,
+		FirstName: "Dave",
+		LastName:  "Olowo",
+		Class:     "11A",
+		Subject:   "Accounting",
+	}
+}
+
+func getTeachers(w http.ResponseWriter, r *http.Request) {
+
+	path := strings.TrimPrefix(r.URL.Path, "/teachers/")
+	idStr := strings.TrimSuffix(path, "/")
+
+	fmt.Println(idStr)
+
+	if idStr == "" {
+		firstName := r.URL.Query().Get("first_name")
+		lastName := r.URL.Query().Get("last_name")
+
+		fmt.Println(firstName, lastName)
+
+		// To change teacher to a slice
+		teacherList := make([]Teacher, 0, len(teachers))
+		for _, teacher := range teachers {
+			if (firstName == "" || teacher.FirstName == firstName) &&
+				(lastName == "" || teacher.LastName == lastName) {
+				teacherList = append(teacherList, teacher)
+			}
+		}
+
+		response := struct {
+			Status string    `json:"status"`
+			Count  int       `json:"count"`
+			Data   []Teacher `json:"data"`
+		}{
+			Status: "success",
+			Count:  len(teacherList),
+			Data:   teacherList,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+
+	// To handle path parameter
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	teacher, exists := teachers[id]
+
+	if !exists {
+		http.Error(w, "Teacher not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(teacher)
+}
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Server is Working!"))
 }
 
 func teacherHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		w.Write([]byte("Hello GET Method on Teachers Route is Working!"))
-		return
+	switch r.Method {
+	case http.MethodGet:
+		// w.Write([]byte("Hello GET Method on Teachers Route is Working!"))
+		getTeachers(w, r)
 	}
 }
-
 func studentsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Students Routes is Working!"))
 }
@@ -41,11 +137,11 @@ func main() {
 
 	mux.HandleFunc("/", rootHandler)
 
-	mux.HandleFunc("/teachers", teacherHandler)
+	mux.HandleFunc("/teachers/", teacherHandler)
 
-	mux.HandleFunc("/students", studentsHandler)
+	mux.HandleFunc("/students/", studentsHandler)
 
-	mux.HandleFunc("/execs", execsHandler)
+	mux.HandleFunc("/execs/", execsHandler)
 
 	fmt.Println("Server Listening on port: ", port)
 
@@ -54,18 +150,19 @@ func main() {
 		MinVersion: tls.VersionTLS12,
 	}
 
-	// Initialize the rate limiter
-	rl := mw.NewRateLimiter(5, time.Minute)
+	// // Initialize the rate limiter
+	// rl := mw.NewRateLimiter(5, time.Minute)
 
-	hppOptions := mw.HPPOptions{
-		CheckQuery:                  true,
-		CheckBody:                   true,
-		CheckBodyOnlyForContentType: "application/x-www-form-urlencoded",
-		WhiteList:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
-	}
+	// hppOptions := mw.HPPOptions{
+	// 	CheckQuery:                  true,
+	// 	CheckBody:                   true,
+	// 	CheckBodyOnlyForContentType: "application/x-www-form-urlencoded",
+	// 	WhiteList:                   []string{"sortBy", "sortOrder", "name", "age", "class"},
+	// }
 
 	// secureMux := mw.Cors(rl.RateLimiterMiddleware(mw.ResponseTimeMiddleWare(mw.SecurityHeaders(mw.Compression(mw.Hpp(hppOptions)(mux))))))
-	secureMux := applyMiddlewares(mux, mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeaders, mw.ResponseTimeMiddleWare, rl.RateLimiterMiddleware, mw.Cors)
+	// secureMux := applyMiddlewares(mux, mw.Hpp(hppOptions), mw.Compression, mw.SecurityHeaders, mw.ResponseTimeMiddleWare, rl.RateLimiterMiddleware, mw.Cors)
+	secureMux := mw.SecurityHeaders(mux)
 
 	server := &http.Server{
 		Addr:    port,
@@ -84,7 +181,7 @@ func main() {
 // Middleware is a function that wraps an http.Handler with additional functionality
 type Middleware func(http.Handler) http.Handler
 
-func applyMiddlewares(handler http.Handler, middlewares ...Middleware) http.Handler {
+func ApplyMiddlewares(handler http.Handler, middlewares ...Middleware) http.Handler {
 	for _, middleware := range middlewares {
 		handler = middleware(handler)
 	}
