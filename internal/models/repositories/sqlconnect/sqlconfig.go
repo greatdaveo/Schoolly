465fb8dd5f39435aca87c259e64862532221dc72
David Olowomeye <olowomeyedavid242@gmail.com>
2025-06-27 03:47:17 +0100

    Integrated .env file

--- internal/models/repositories/sqlconnect/sqlconfig.go
+++ internal/models/repositories/sqlconnect/sqlconfig.go
@@ -22,7 +22,6 @@ func ConnectDB() (*sql.DB, error) {
 	DB_PORT := os.Getenv("DB_PORT")
 	host := os.Getenv("HOST")
 
-	// connectionString := "root:SQLadmin12345!@tcp(127.0.0.1:3306)/" + dbname
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, DB_PORT, dbname)
 	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
